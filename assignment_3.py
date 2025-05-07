4eb7848192bd31c3bf45c7c01d38a856a1a8745f
Max Andreasen <maxandr@kth.se>
2025-05-07 14:58:50 +0200

    initialization and setup

---
+++ assignment_3.py
@@ -0,0 +1,30 @@
+
+'''
+Assignment #3 in DD2424 Deep Learning in Data Science.
+By Max Andreasen.
+
+In this assignment the aim is to construct and implement a Convolutional Neural Network.
+'''
+
+class CNN:
+    def __init__(self, lr=0.01, lam=0):
+        # Network parameters.
+        self.lr = lr
+        self.lam = lam
+
+        # The model will hold the data.
+        self.data = None
+        self.labels = None
+
+    def process_data(self, file):
+        return
+
+    def softmax(self, s):
+        return
+
+    def cross_entropy_loss(self):
+        return
+
+    def forward_pass(self):
+        return
+

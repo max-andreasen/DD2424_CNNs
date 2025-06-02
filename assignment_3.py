implemented MX computation, convolution using MX and tests for the functionality. Found a bug in the 'ground truth' forward pass, but know what is wrong. Just need to find a fix.

assignment_3.py
@@ -8,9 +8,17 @@ In this assignment the aim is to construct and implement a Convolutional Neural 
 
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 class CNN:
-    def __init__(self, X, Y, y, m=100, lr=0.01, lam=0, stride=2, W=None, B=None, n_f=None, n_p=None, filters=None, K=10):
+    '''
+    The class is currently built around handling square images,
+    where WIDTH == HEIGHT.
+    '''
+
+    def __init__(self, X, Y, y, m=100, lr=0.01, lam=0, stride=2, n_batches=10,
+                 W=None, B=None, filters=None, n_filters=2, K=None,
+                 init_MX=True):
 
         self.X = X                  # X is the dataset consisting of images, y labels.
         self.Y = Y
@@ -23,10 +31,9 @@ class CNN:
         self.n_images = X.shape[-1]
 
         # Network parameters.
-        self.L = 2                  # We hardcode a 2 layer network for simplicity.
-        self.m = m                  # Hidden layer dimensions (also referred to as 'd').
-        self.K = K                  # The number of classes.
-        self.MX = None              # Initialized to None. Calculated in the forward pass.
+        self.L = 2                          # We hardcode a 2 layer network for simplicity.
+        self.m = m                          # Hidden layer dimensions (also referred to as 'd').
+        self.K = K if K is not None else len(set(y))   # The number of classes.
 
         # Network parameters
         self.lr = lr                # The initial learning rate of the model.
@@ -40,21 +47,23 @@ class CNN:
         self.grads = {}
 
         # CNN parameters
-        self.stride = stride        # Also referred to as 'f'.
-        self.filters = filters      # A list with different filters (numpy arrays).
-        self.n_f = n_f              # Number of filters.
-        self.n_p = n_p              # Number of sub-patches to which the filter is applied.
+        self.stride = stride            # Also referred to as 'f'.
+        self.filters = filters if filters is not None else np.zeros((self.stride, self.stride, 3, n_filters)) # shape (f, f, 3)
+        self.n_f = self.filters.shape[-1]                   # Could also use 'n_filters', but unclear if 'filters' is given.
+        self.n_p = (self.width // self.stride) ** 2     # Number of sub-patches to which the filter is applied.
+        self.filters_flat = self.filters.reshape( (self.stride * self.stride * 3, self.n_f), order='C')
 
-        # The model will hold the data.
-        self.data = None
-        self.labels = None
+        # Initializes variables
+        if init_MX:
+            self.MX = self.construct_MX(self.X)
+        else:
+            self.MX = None
 
 
 
     # -----------------------------------------------
     #  Helper functions
     # -----------------------------------------------
-
     def process_data(self, file):
         return
 
@@ -66,15 +75,16 @@ class CNN:
         return
 
     def convolve(self, X, conv_filter, stride=None):
+        '''
+        Used for the 'ground truth' forward pass.
+        '''
         if stride is None:
             stride = self.stride
         f = conv_filter.shape[0]                # Enough with width since filter is a square.
         H_out = (X.shape[0] - f) // stride + 1  # Height of the new image.
         W_out = (X.shape[1] - f) // stride + 1  # Width of the new image.
         conv_out = np.zeros( (H_out, W_out), dtype=X.dtype)   # The outputted image shape.
-
         assert X.shape[2] == conv_filter.shape[2], f"Depth of filter and image is not equal"
-
         # Loops through and creates each sub-patch.
         for i in range(H_out):
             for j in range(W_out):
@@ -82,24 +92,37 @@ class CNN:
                 conv_out[i, j] = np.sum(patch * conv_filter)
         return conv_out
 
+    def convolve_efficient(self, X_batch):
+        if self.MX is None:
+            self.MX = self.construct_MX(X_batch)
+        assert self.filters_flat.shape == (self.stride * self.stride * 3, self.n_f), f"Filters are of wrong shape: {self.filters_flat.shape}"
+        n_img_batch = X_batch.shape[-1]
+        conv_outputs_mat = np.zeros((self.n_p, self.n_f, n_img_batch))
+        for i in range(n_img_batch):
+            conv_outputs_mat[:, :, i] = np.matmul(self.MX[:, :, i], self.filters_flat)
+        conv_outputs_mat = np.einsum('ijn, jl->iln', self.MX, self.filters_flat, optimize=True)
+        return conv_outputs_mat
 
-
-    # -----------------------------------------------
-    #  Initializations
-    # -----------------------------------------------
-    def init_network(self):
-        self.filters = [np.zeros((self.stride, self.stride, 3)) * 5] # shape (f, f, 3)
-        self.n_f = len(self.filters)
-        self.n_p = (self.X.shape[0] / self.stride) ** 2
-        self.K = len(set(self.y))
-
-        d0 = self.n_f * self.n_p        # The shape after flattening the feature vector (after convolution).
-
-        self.W[0] = np.zeros( (self.m, d0) )
-        self.W[1] = np.zeros( (self.K, self.m) )
-
-        assert self.W[0].shape == (self.m, d0), f"W[0] initialized to the wrong shape of: {self.W.shape}"
-        assert self.W[1].shape == (self.K, self.m)
+    def construct_MX(self, X):
+        '''
+        THIS FUNCTION ASSUMES A SQUARE IMAGE (WIDTH == HEIGHT).
+        :param X:
+        :return:
+        '''
+        print(f"Calculating MX...")
+        start = time.time()
+        MX = np.zeros((self.n_p, self.stride * self.stride * 3, self.n_images))
+        for i in range(self.n_images):
+            X_img = X[:, :, :, i]
+            patch_id = 0
+            for r in range(int(np.sqrt(self.n_p))):
+                for col in range(int(np.sqrt(self.n_p))):
+                    X_patch = X_img[ r*self.stride:(r+1)*self.stride, col*self.stride:(col+1)*self.stride, :]
+                    MX[patch_id, :, i] = X_patch.reshape((1, self.stride * self.stride * 3), order='C')
+                    patch_id += 1
+        end = time.time()
+        print(f"MX calculated in {end - start} seconds.")
+        return MX
 
 
 
@@ -112,27 +135,33 @@ class CNN:
 
 
     def forward_efficient(self, X_batch, return_testing=False):
-        return
+        assert self.filters_flat.shape == (
+        self.stride * self.stride * 3, self.n_f), f"Filters are of wrong shape: {self.filters_flat.shape}"
+
+        # NOTE: Also fills MX if MX is empty!
+        conv_outputs_mat = self.convolve_efficient(X_batch)
+
 
 
     def forward_pass(self, X_batch, return_testing=False):
-
         hs = []
         Ps = []
         x1s = []
         for j in range(X_batch.shape[-1]):
-            X_img=X_batch[:,:,:,j]
+            X_img = X_batch[:,:,:,j]
             # h is a collection of all response maps.
             h = np.zeros((self.n_f * self.n_p, 1), dtype=X_batch.dtype)
+            H_all = np.zeros( (self.height // self.stride, self.width // self.stride, self.n_f), dtype=X_batch.dtype)
             for i in range(self.n_f):
                 H_i = np.maximum(0, self.convolve(X_img, self.filters[:, :, :, i]))     # Applies ReLu.
-                assert H_i.shape == (32 // self.stride, 32 // self.stride), f"H_i is of wrong shape in forward pass: {H_i.shape}"
+                assert H_i.shape == (self.width // self.stride, self.height // self.stride), f"H_i is of wrong shape in forward pass: {H_i.shape}"
                 start = i * self.n_p
                 end = (i + 1) * self.n_p
                 h[start:end, 0] = H_i.reshape(-1)  # Flatten H_i to shape (n_p,) to create vertically stacked vectors.
+                H_all[:, :, i] = H_i
 
+            h = np.transpose(H_all, (2, 0, 1)).reshape(-1, 1)
             assert h.shape == (self.n_f * self.n_p, 1), f"h is of wrong shape in forward pass: {h.shape}"
-
             x1 = np.maximum(0, self.W[0] @ h + self.B[0])   # Applies ReLu.
             x1s.append(x1)
             s = self.W[1] @ x1 + self.B[1]
@@ -142,11 +171,13 @@ class CNN:
             hs.append(h)
 
         P = np.concatenate(Ps, axis=1)
+        hs = np.concatenate(hs, axis=1)
+        X1 = np.expand_dims(np.concatenate(x1s, axis=1), axis=0)
         if return_testing:
             return {
                 'P': P,
                 'h': hs,
-                'x1': x1s,
+                'x1': X1,
             }
         return P
 

test_assignment_3.py
@@ -15,6 +15,20 @@ class TestCNN(unittest.TestCase):
     B2 = samples.b2
     P = samples.P  # The softmax prediction of the first image.
     Fs = samples.Fs  # The filters (there are 2).
+    MX = samples.MX
+
+    x1 = samples.X1
+    conv_outputs = samples.conv_outputs
+    conv_flat = samples.conv_flat
+    conv_outputs_mat = samples.conv_outputs_mat
+
+    stride = Fs.shape[0]
+    n_p = (32 // stride) ** 2
+
+    X = np.transpose(X.reshape((32, 32, 3, 5), order='F'), (1, 0, 2, 3))
+
+    cnn = CNN(X=X, Y=Y, y=y, W=[W1, W2], B=[B1, B2], stride=stride,
+              filters=Fs, K=10, init_MX=False)
 
     # CONVOLUTION
     def test_single_filter_convolution(self):
@@ -24,52 +38,55 @@ class TestCNN(unittest.TestCase):
             [[0, 1], [1, 2], [2, 1]],
             [[1, 0], [0, 1], [1, 1]]
         ])
-
         # Filter: 2x2.
         conv_filter = np.array([
             [[1, 0], [0, 1]],
             [[1, -1], [0, 1]]
         ])
-
-        stride = 1
-
         # Manual calculation.
         expected_output = np.array([
             [2, 3],
             [4, 2]
         ])
 
-        cnn = CNN(X=X, Y = None, y=None)
+        stride = 1
+        cnn = CNN(X=X, Y=None, y=None, K=10, init_MX=False)
         output = cnn.convolve(X, conv_filter, stride)
 
         self.assertEqual(output.shape, expected_output.shape)
         np.testing.assert_allclose(output, expected_output)
 
+    def test_convolve_efficient(self):
+        out_conv_outputs_mat = self.cnn.convolve_efficient(self.X)
+        self.assertEqual(self.conv_outputs_mat.shape, out_conv_outputs_mat.shape)
+        self.assertEqual(self.conv_outputs_mat.dtype, out_conv_outputs_mat.dtype)
+        np.testing.assert_allclose(self.conv_outputs_mat, out_conv_outputs_mat, rtol=1e-5, atol=1e-8)
+
+    def test_construct_MX(self):
+        out_MX = self.cnn.construct_MX(self.X)
+        self.assertEqual(self.MX.shape, out_MX.shape)
+        self.assertEqual(self.MX.dtype, out_MX.dtype)
+        np.testing.assert_allclose(self.MX, out_MX, rtol=1e-5, atol=1e-8)
+
 
     # FORWARD PASSES
     def test_forward_pass(self):
-        # Don't need to test these. Maybe I will for good practice if I have time left.
-        X1 = self.samples.X1
-        conv_outputs = self.samples.conv_outputs[:, :, :, 0] # H_i
-
-        stride = self.Fs.shape[0]            # Calculating the stride.
-        n_p = (32 // stride) ** 2
-        X = np.transpose( self.X.reshape((32, 32, 3, 5), order='F'), (1,0,2,3) )
-
-        cnn = CNN(X=X, Y=self.Y, y=self.y, K=10, W=[self.W1, self.W2], B=[self.B1, self.B2], stride=stride, n_f=self.samples.nf, n_p=n_p, filters=self.Fs)
-
         # Running the forward pass to see if any errors occur within the forward pass.
-        outputs = cnn.forward_pass(X, return_testing=True)
+        outputs = self.cnn.forward_pass(self.X, return_testing=True)
         self.assertEqual(outputs['P'].shape, self.P.shape)
+        self.assertEqual(outputs['P'].dtype, self.P.dtype)
+        self.assertEqual(outputs['x1'].shape, self.x1.shape)
+        self.assertEqual(outputs['h'].shape, self.conv_flat.shape)
 
+        print(outputs['h'][:10, 0])
+        print(self.conv_flat[:10, 0])
+
+        np.testing.assert_allclose(outputs['h'], self.conv_flat, rtol=1e-5, atol=1e-8)
+        np.testing.assert_allclose(outputs['x1'], self.x1, rtol=1e-5, atol=1e-8)
+        np.testing.assert_allclose(outputs['P'], self.P, rtol=1e-5, atol=1e-8)
 
     def test_forward_efficient(self):
-        X = np.transpose(self.X.reshape((32, 32, 3, 5), order='F'), (1, 0, 2, 3))
-        stride = self.Fs.shape[0]
-        n_p = (X.shape[0] // stride) ** 2
-
-        cnn = CNN(X=X, Y=self.Y, y=self.y, K=10, W=[self.W1, self.W2], B=[self.B1, self.B2], stride=stride, n_f=self.samples.nf, n_p=n_p, filters=self.Fs)
-        outputs = cnn.forward_efficient(X, return_testing=True)
+        outputs = self.cnn.forward_efficient(self.X, return_testing=True)
 
 
 if __name__ == "__main__":